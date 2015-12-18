349905d69acf80973ec4d83cc1c58df3294ac668
Kevin <kevinkuhn13@gmail.com>
2015-12-18 19:52:00 +0100

    count different tags funciton

--- setup.py
+++ setup.py
@@ -14,3 +14,17 @@ import string
 # Import XML/OSM file into a directory
 file_path = "source\lucerne.osm"
 osm_file = open(file_path, "r")
+
+# Find all the different tags within the dataset
+# return them into a dictonary
+def count_tags(filename):
+    tag_count = {}
+    for _, element in ET.iterparse(filename, events=("start",)):
+        add_tag(element.tag, tag_count)
+    return tag_count
+
+def add_tag(tag, tag_count):
+    if tag in tag_count:
+        tag_count[tag] += 1
+    else:
+        tag_count[tag] = 1
