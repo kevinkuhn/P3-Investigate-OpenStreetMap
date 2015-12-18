9b79dc126c1dda21ce4d18442132467b2c5c73f0
Kevin <kevinkuhn13@gmail.com>
2015-12-18 21:39:28 +0100

    def shape_elements

--- data.py
+++ data.py
@@ -10,8 +10,60 @@ lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
-CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
+setup = [ "version", "changeset", "timestamp", "user", "uid"]
 
 def is_address(elem):
     if elem.attrib['k'][:5] == "addr:":
         return True
+
+def shape_element(element):
+    node = {}
+    if element.tag == "node" or element.tag == "way" :
+        for key in element.attrib.keys():
+            val = element.attrib[key]
+            node["type"] = element.tag
+            if key in setup:
+                if not "created" in node.keys():
+                    node["created"] = {}
+                node["created"][key] = val
+            elif key == "lat" or key == "lon":
+                if not "pos" in node.keys():
+                    node["pos"] = [0.0, 0.0]
+                old_pos = node["pos"]
+                if key == "lat":
+                    new_pos = [float(val), old_pos[1]]
+                else:
+                    new_pos = [old_pos[0], float(val)]
+                node["pos"] = new_pos
+            else:
+                node[key] = val
+            for tag in element.iter("tag"):
+                tag_key = tag.attrib['k']
+                tag_val = tag.attrib['v']
+                if tag_key == "addr:street":
+                    # check if steet name fits to conventions (function update_name)
+                    tag_val = update_name(tag_val)
+                if problemchars.match(tag_key):
+                    continue
+                elif tag_key.startswith("addr:"):
+                    if not "address" in node.keys():
+                        node["address"] = {}
+                    addr_key = tag.attrib['k'][len("addr:") : ]
+                    if lower_colon.match(addr_key):
+                        continue
+                    else:
+                        node["address"][addr_key] = tag_val
+                elif lower_colon.match(tag_key):
+                    node[tag_key] = tag_val
+                else:
+                    node[tag_key] = tag_val
+        for tag in element.iter("nd"):
+            if not "node_refs" in node.keys():
+                node["node_refs"] = []
+            node_refs = node["node_refs"]
+            node_refs.append(tag.attrib["ref"])
+            node["node_refs"] = node_refs
+
+        return node
+    else:
+        return None
