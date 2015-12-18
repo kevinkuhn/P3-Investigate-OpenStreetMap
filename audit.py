bcc3768682b1b078340916551b13096c9a0fd15b
Kevin <kevinkuhn13@gmail.com>
2015-12-18 20:02:01 +0100

    Link to osm file & expected street name ending

--- audit.py
+++ audit.py
@@ -2,3 +2,12 @@ import xml.etree.cElementTree as ET
 from collections import defaultdict
 import re
 import pprint
+
+OSMFILE = "source\lucerne.osm"
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+# Array with all the expected steet name endings
+expected = ["strasse","Strasse", "platz",
+			"weg","gasse","matt","matte",
+			"halde", "quai", "ring", "hof",
+			"weid"]
