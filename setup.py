c79982a0370be85392ca1e5cb108f1d78e430aaa
Kevin <kevinkuhn13@gmail.com>
2015-12-18 19:51:21 +0100

    import libraries

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Import libraries
+import xml.etree.cElementTree as ET
+from collections import defaultdict
+import re
+import pprint
+import codecs
+from codecs import open
+import json
+import string
