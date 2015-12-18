6d207847742a0b5efcd175d34db9b8438d99b521
Kevin <kevinkuhn13@gmail.com>
2015-12-18 21:36:33 +0100

    compiler, created vars and def is_address

--- data.py
+++ data.py
@@ -5,3 +5,13 @@ import pprint
 import re
 import codecs
 import json
+
+lower = re.compile(r'^([a-z]|_)*$')
+lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
+
+def is_address(elem):
+    if elem.attrib['k'][:5] == "addr:":
+        return True
