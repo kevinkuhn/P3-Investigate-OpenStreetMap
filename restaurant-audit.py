3df96722acc75938087ae1af5dd9e744a17e794e
Kevin <kevinkuhn13@gmail.com>
2015-12-22 17:28:58 +0100

    Changed Mapping, Added Codec check, Launched update_name

--- restaurant-audit.py
+++ restaurant-audit.py
@@ -7,52 +7,46 @@ import pprint
 import sys
 
 OSMFILE = "source\lucerne.osm"
-street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+restaurant_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 
 # Array with all the expected steet name endings
-expected = ["strasse", "platz", "weg",
-			"allee","gasse","matt","matte",
-			"halde", "quai", "ring", "hof",
-			"weid", "höhe", "wil", "egg"]
+expected = []
 
 # mapping definitions
-mapping = { "str." : "strasse",
-            "Alee": "Allee",
-            "Alle": "Allee",
-            u"gäsli" : u"gässli",
-            u"paradiesgässli" : u"Paradiesgässli"
+mapping = { "Chimney?s" : "Chimeney's",
+            "felsenegg": "Felsenegg",
+            "KAFFEEPLUS": "KaffeePlus",
+            "Mill?Feuille": "Mill'Feuille"
             }
 
 # Create an audit for street names that do not end with the expected street name endings
 
 def audit_restaurant_types(restaurant_types, street_name):
-    m = street_type_re.search(street_name)
+    m = restaurant_type_re.search(street_name)
     # if street_type_re has found an element
     if m:
-        street_type = m.group() 
+        rest_type = m.group() 
         # loop to check if the steet name ending matches with one of the endings for the expected list
-        for check not in expected:
-            restaurant_types[street_type].add(street_name)
+        #for check not in expected:
+        restaurant_types[rest_type].add(street_name)
                    
 
 # Returns the k attribute for XML list if it equal to a street address
 def is_restaurant(elem):
     return (elem.attrib['k'] == "amenity")
-'''
-def set_utf8(message):
-    import sys
-    # Change message into unicode and compile it with the defined charset
-    return message.decode("utf-8").encode(sys.stdout.encoding)
-'''
 
-'''
+def has_name(elem):
+    return (elem.attrib['k'] == "name")
+
 def update_name(name, mapping):
-    st_type = street_type_re.search(name).group()
+    st_type = restaurant_type_re.search(name).group()
     for x in mapping:
         if name.find(x)!=-1:
-            name =  name.replace(x,mapping[x])
+            new_name =  name.replace(x,mapping[x])
+            print name,"=>",new_name
+            name = new_name
     return name
-'''
+
 # Create an audit an check with functions above
 def audit(osmfile):
     osm_file = open(osmfile, "r")
@@ -62,12 +56,26 @@ def audit(osmfile):
         # search withing the python object for elements with the tag property == "way"
         if elem.tag == "node" or elem.tag == "amenity":
             # loop just within elements with <tag ... >
+            setPass = False
+            # Encoding of system
+            stdout_encoding = sys.stdout.encoding or sys.getfilesystemencoding()
+
             for tag in elem.iter("tag"):
+                if setPass == True:
+                    if tag.attrib['k'] == 'name':
+                        # Print restaurant name with replacing missing/wrong chracters
+                        #print tag.attrib['v'].encode(stdout_encoding, errors='replace')
+                        update_name(tag.attrib['v'].encode(stdout_encoding, errors='replace'),mapping)
+                    setPass = False
                 # if the k attribute of the tag is a street address then continue to audit
+                if tag.attrib['k'] == 'amenity' and tag.attrib['v'] == 'restaurant' :
+                    setPass = True
+
                 if is_restaurant(tag):
-                    # audit checks if it is an expected or unexpected street name based on attribute v
-                    audit_restaurant_types(restaurant_types, tag.attrib['v'])
-                    print restaurant_types
+                    if has_name(tag):
+                        # audit checks if it is an expected or unexpected street name based on attribute v
+                        audit_restaurant_types(restaurant_types, tag.attrib['v'])
+                        print restaurant_types
     # print out the list of unexpected street names as a dictonary
     #pprint.pprint(dict(restaurant_types))
     # print the number of streets that do not fit to the expectations
@@ -78,14 +86,14 @@ def test():
     st_types = audit(OSMFILE)
     #assert len(st_types) == 3
     #pprint.pprint(dict(st_types))
-    '''
+    
     for st_type, ways in st_types.iteritems():
                     for name in ways:
                         better_name = update_name(name, mapping)
                         print name, "=>", better_name
                         if name == "paradiesgässli":
                             assert better_name == "Paradiesgässli"
-    '''
+    
 
 
 if __name__ == '__main__':
